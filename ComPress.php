9f64ff57db98cbbe4cf0e757cfd4d614057e9e75
Séra Bálint <sera.balint@e-vista.hu>
2015-11-22 11:58:16 +0100

    Services moved to a separate service definition file (services.yml)

--- ComPress.php
+++ ComPress.php
@@ -1,6 +1,7 @@
 <?php
 namespace Evista\ComPress;
 
+use Symfony\Component\Yaml\Yaml;
 
 include_once('vendor/autoload.php');
 
@@ -28,80 +29,10 @@ class ComPress {
      *
      */
     public function setUpServices(){
-
-        $this->services = [
-
-            'form.builder' => [
-                'class' => 'AdamWathan\Form\FormBuilder',
-            ],
-
-            'yaml' => [
-                'class' => 'Symfony\Component\Yaml\Yaml',
-            ],
-
-            /**
-             * Swiftmailer mail
-             * composer require swiftmailer/swiftmailer
-             * https://packagist.org/packages/swiftmailer/swiftmailer
-             *
-             * Usage:
-             * $message = \Swift_Message::newInstance();
-             * $message
-             * ->setBody('<h1>Test</h1>')
-             * ->setSubject("test")
-             * ->setFrom('info@e-vista.hu')
-             * ->setTo('balint.sera@gmail.com');
-             * $mailer->send($message);
-             */
-            /**
-            'swiftmailer.transport' => [
-                'class' => '\Swift_SmtpTransport',
-                'method' => 'newInstance',
-                'arguments' => ['127.0.0.1', 25]
-            ],
-
-            'swiftmailer.mailer' => [
-                'class' => '\Swift_Mailer',
-                'method' => 'newInstance',
-                'arguments' => ['@swiftmailer.transport']
-            ],
-            */
-            /**
-             * Twig templating:
-             * composer require twig/twig
-             * https://packagist.org/packages/twig/twig
-             *
-             * Usage: echo $container->get('twig.templating')->render('test.html.twig', ['hehh' => 'Yeah!' ]);
-
-             */
-            'twig.loader' =>[
-                'class' => '\Twig_Loader_Filesystem',
-                'arguments' => [$this->getTwigTemplateDir()]
-            ],
-            'twig.templating' => [
-                'class' => '\Twig_Environment',
-                'arguments' => ['@twig.loader']
-            ],
-
-
-
-            // These are here for testing purposes. Feel free to remove them
-            'test.service' => [
-                'class' => 'Evista\ComPress\Service\TestService',
-                'arguments' => ['@test.service.param1', '@test.service.param2']
-            ],
-            'test.service.param1' => [
-                'class' => 'Evista\ComPress\Service\TestService2',
-                'arguments' => ['@test.service.param2', ['testarray'=>'testparam']]
-            ],
-            'test.service.param2' => [
-                'class' => 'Evista\ComPress\Service\TestService3',
-                'arguments' => ['stringarg']
-            ],
-        ];
+        $this->services = Yaml::parse(file_get_contents(__DIR__.'/services.yml'));
     }
 
-    private function getTwigTemplateDir(){
+    public static function getTwigTemplateDir(){
         // Get current template for setup twig's view directory
         $currentTheme = wp_get_theme();
         $currentThemeDir = $currentTheme->theme_root.'/'.$currentTheme->template;
